0aa07bf7daba3237767912ab53173e21ac74ea14
Jinwoo Lee <starday456@kaist.ac.kr>
2022-03-19 21:25:37 +0900

    problem template for state chapter

---
+++ state/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const DOG_IMG_URL =
+  'https://www.princeton.edu/sites/default/files/styles/half_2x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=iQEwihUn';
+const CAT_IMG_URL =
+  'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1100-c50.jpg';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3>Do you like dog, or cat?</h3>
+      {/* TODO: button */}
+      {/* TODO: image */}
+    </div>
+  );
+};
+
+export default App;
