fa0a7c371c758bf8c7191478c000ac80ed1abb1d
Jinwoo Lee <starday456@kaist.ac.kr>
2022-03-19 19:39:10 +0900

    Add namecard example

---
+++ component-basic/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const myName = '';
+const myAge = 0;
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3>Name card</h3>
+    </div>
+  );
+};
+
+export default App;
